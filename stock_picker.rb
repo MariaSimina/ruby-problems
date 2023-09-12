539a9d2fe5879f448813776adf94caf935321ff4
Maria Simina <maria.simina_groza@yahoo.com>
2023-09-12 13:25:01 +0300

    Solve simpler, without hash

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,4 +1,6 @@
 def stock_picker(array)
+    greatest_difference = 0
+    result_array = []
     diff = {}
 
     (0..array.length-1).each do |index|
@@ -9,13 +11,17 @@ def stock_picker(array)
             if second_index <= index
                 next
             end
+            
             difference = array[index] - array[second_index]
-            diff["#{index}, #{second_index}"] = difference unless difference >= 0
+            if greatest_difference > difference
+                greatest_difference = difference
+                result_array[0] = index
+                result_array[1] = second_index
+            end
         end
     end
-    sorted = diff.sort_by {|key, value| value }.first
-    result = sorted[0].split(", ").map{ |number| number.to_i}
-    p result
+    p greatest_difference
+    p result_array
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
