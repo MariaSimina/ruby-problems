ec3c99e5abad5dc02728c4388511378def7e5807
Maria Simina <maria.simina_groza@yahoo.com>
2023-09-12 13:34:31 +0300

    Add comments

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,13 +1,14 @@
 def stock_picker(array)
     greatest_difference = 0
     result_array = []
-    diff = {}
 
     (0..array.length-1).each do |index|
         (1..array.length-1).each do |second_index|
+            #so that it doesn't go over the array with the second_index
             if index == array.length - 1
                 next
             end
+            #so that it doesn't compare with days before the day of the buy
             if second_index <= index
                 next
             end
@@ -20,8 +21,7 @@ def stock_picker(array)
             end
         end
     end
-    p greatest_difference
-    p result_array
+    result_array
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
